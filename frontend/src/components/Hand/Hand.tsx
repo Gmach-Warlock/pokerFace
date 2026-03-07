509d34604923ad48e3ccec6bdcda9879e4f49a6a
Gary Yamashita <garyjyam@gmail.com>
2026-03-07 06:05:49 -0800

    backend created, assets moved into backend

--- frontend/src/components/Hand/Hand.tsx
+++ frontend/src/components/Hand/Hand.tsx
@@ -4,21 +4,21 @@ import "./Hand.css";
 export default function Hand() {
   return (
     <div className="hand-container place-center">
-      <div className="hand-draw place-center">
+      <div className="hand-draw place-center w-full">
         <div className="hand-draw-card1">
-          <Card value={`A`} suit="spade" side="face-up" />
+          <Card value={`A`} suit="heart" side="face-up" />
         </div>
         <div className="hand-draw-card2">
-          <Card value={`K`} suit="spade" side="face-up" />
+          <Card value={`K`} suit="heart" side="face-up" />
         </div>
         <div className="hand-draw-card3">
-          <Card value={`Q`} suit="spade" side="face-up" />
+          <Card value={`Q`} suit="heart" side="face-up" />
         </div>
         <div className="hand-draw-card4">
-          <Card value={`J`} suit="spade" side="face-up" />
+          <Card value={`J`} suit="heart" side="face-up" />
         </div>
         <div className="hand-draw-card5">
-          <Card value={10} suit="spade" side="face-up" />
+          <Card value={10} suit="heart" side="face-up" />
         </div>
       </div>
     </div>
